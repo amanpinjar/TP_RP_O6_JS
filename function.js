16f998ee7d0878e3cb5a9c18e1ae2f1c24924c79
Aman nabi rasul pinjar <pointpart32@gmail.com>
2024-04-29 18:30:06 +0530

    c66

--- function.js
+++ function.js
@@ -4,57 +4,52 @@ function greet (name){
   greet("aman")
   
   
-  // Function to calculate the area of a rectangle
-  function calcArea(width, height) {
-    var area = width * height; // Calculate the area by multiplying length (a) by width (b)
-    console.log(`Area of Rectangle is :- ${area}cm`); // Output the result
+  // Function  calculate the area of a rectangle
+  function calcArea(w, h) {
+    let area = w * h; 
+    console.log(`the Area of Rectangle is :- ${area}inch`); 
   }
+  calcArea(10, 14); 
   
   // Function to convert Fahrenheit to Celsius
-  function fahrenheitToCelsius(F) {
-    var convert = (F - 32) * 5/9; // Fahrenheit to Celsius using the formula
-    console.log(parseInt(convert), "C"); // Output the result
-  }
+  function fahrenheitToCelsius(fahrenheit) {
+    var celsius = (fahrenheit - 32) * 5/9;
+    return celsius;
+}
+fahrenheitToCelsius(45); 
   
   // Function to convert Celsius to Fahrenheit
-  function celsiusToFahrenheit(C) {
-    var convert = (C * 9/5) + 32; //  Celsius to Fahrenheit using the formula
-    console.log(parseInt(convert), "F"); // Output the result
-  }
+  function celsiusToFahrenheit(celsius) {
+    var fahrenheit = (celsius * 9/5) + 32;
+    return fahrenheit;
+}
+celsiusToFahrenheit(34); 
   
-  // Example usage of the functions:
-  calcArea(10, 14); // Calculate the area of a rectangle with width 10 and height 14
-  fahrenheitToCelsius(-459.67); // Convert -459.67°F to Celsius
-  celsiusToFahrenheit(0); // Convert 0°C to Fahrenheit
+
   
-  // Calculator function
+  
   function add(a, b) {
     return a + b;
   }
+  console.log(add(7, 6));
   
-  // Function to subtract two numbers
+ 
   function subtract(a, b) {
     return a - b;
   }
+  console.log(subtract(7, 6));
   
-  // Function to multiply two numbers
+  
   function multiply(a, b) {
     return a * b;
   }
+  console.log(multiply(7, 6));
   
-  // Function to divide two numbers
+  
   function divide(a, b) {
     return a / b;
   }
+  console.log(divide(7, 6));
   
-  // Call add function
-  console.log(add(5, 3)); // Output: 8
-  
-  // Call subtract function
-  console.log(subtract(5, 3)); // Output: 2
-  
-  // Call multiply function
-  console.log(multiply(5, 3)); // Output: 15
-  
-  // Call divide function
-  console.log(divide(5, 3)); // Output: 1.6666
+
+
